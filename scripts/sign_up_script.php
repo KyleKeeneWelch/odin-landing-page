6cc2888a3fc9b0dc00099b2d120c9aea3f7b1381
KyleKeeneWelch <kylekeene.welch@gmail.com>
2023-05-16 20:54:20 +0100

    Add save user to newsletter text file
    Filling in the fields on the sign up page and pressing submit will validate the input and will save it to a file named newsletter.text.

--- scripts/sign_up_script.php
+++ scripts/sign_up_script.php
@@ -3,32 +3,40 @@
 $nameErr = $emailErr = $contact_numberErr = $contact_methodErr = "";
 $name = $email = $contact_number = "";
 $contact_method = [];
+$valid = TRUE;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["name"])) {
         $nameErr = "Name is required";
+        $valid = FALSE;
     }
     else {
-        $name = test_input($_POST["name"]);
-
         if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
             $nameErr = "Only letters and white space allowed";
+            $valid = FALSE;
+        }
+        else {
+            $name = test_input($_POST["name"]);
         }
     }
 
     if (empty($_POST["email"])) {
         $emailErr = "Email is required";
+        $valid = FALSE;
     }
     else {
-        $email = test_input($_POST["email"]);
-
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
             $emailErr = "Invalid email format";
+            $valid = FALSE;
+        }
+        else {
+            $email = test_input($_POST["email"]);
         }
     }
 
     if (empty($_POST["contact_number"])) {
         $contact_numberErr = "Contact Number is required";
+        $valid = FALSE;
     }
     else {
         $contact_number = test_input($_POST["contact_number"]);
@@ -36,6 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (empty($_POST["contact_method"])) {
         $contact_methodErr = "At least one contact method is required";
+        $valid = FALSE;
     }
     else {
         if (IsChecked("contact_method", "email")) {
@@ -49,6 +58,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         foreach ($contact_method as $chkval) {
             $chkval = htmlspecialchars($chkval);
         }
+    }
+
+    if ($valid == TRUE) {
+        try {
+            $newsletter = fopen("docs/newsletter.text", "a");
+        } catch (Exception $e) {
+            echo "Unable to open file";
+        }
+
+        $contact_method_str = "";
+        foreach ($contact_method as $method) {
+            $contact_method_str .= $method . "\n";
+        }
+        $date = date("l jS \of F Y h:i:s A");
+
+        $txt = $date . "\n" . $name . "\n" . $email . "\n" . $contact_number . "\n" . $contact_method_str . "\n";
+        try {
+            fwrite($newsletter, $txt);
+            echo "Sign Up Successful";
+        } catch (Exception $e) {
+            echo "Sign Up Unsuccessful";
+        }
+        fclose($newsletter);
+
     }
 }
 
@@ -64,9 +97,13 @@ function IsChecked($chkname, $value) {
 }
 
 function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+    try {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    } catch (Exception $e) {
+        echo "Invalid Input";
+    }
 }
 ?>
